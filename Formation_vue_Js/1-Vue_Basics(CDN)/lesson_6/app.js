c37cde0aed13daa24bceaab3023cfa9e2c28d47b
takykongo <takykongoabdoul@gmail.com>
2022-09-08 15:33:13 +0000

    Other mouse events

---
+++ Formation_vue_Js/1-Vue_Basics(CDN)/lesson_6/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45,
+            x: 0,
+            y: 0
+        }
+    },
+    methods :{
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data)
+            console.log(data)
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+    }
+})
+
+app.mount('#app')
