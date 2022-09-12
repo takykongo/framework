09bbddd0164b0d9453b720229fe2d99d2fd53a5e
takykongo <takykongoabdoul@gmail.com>
2022-09-12 22:39:26 +0000

    Multiple components

---
+++ Formation_vue_Js/2-The_Vue_CLI/modal-project/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>    
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .modal{
+        width: 400px;
+        padding: 50px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+    }
+</style>
