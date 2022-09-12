7e434ff4e8f53ed27b91c5db0039d9a6a40ce375
takykongo <takykongoabdoul@gmail.com>
2022-09-12 23:19:30 +0000

    Component styles & global style

--- Formation_vue_Js/2-The_Vue_CLI/modal-project/src/components/Modal.vue
+++ Formation_vue_Js/2-The_Vue_CLI/modal-project/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal title</h1>
             <p>modal content</p>
         </div>
     </div>    
@@ -24,5 +25,13 @@
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.5);
+    }
+    .modal h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    .modal p{
+    font-style: normal;
     }
 </style>
