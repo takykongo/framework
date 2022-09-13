2eceb3ebaf574578cc7c43451f83dc79cd8e6eaf
takykongo <takykongoabdoul@gmail.com>
2022-09-13 11:31:06 +0000

    Emitting custom Events

--- Formation_vue_Js/2-The_Vue_CLI/modal-project/src/components/Modal.vue
+++ Formation_vue_Js/2-The_Vue_CLI/modal-project/src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="backdrop">
+    <div @click="closeModal" class="backdrop">
         <div class="modal" :class="{sale: theme === 'sale', dark: theme === 'dark'}">
             <h1>{{header}}</h1>
             <p>{{text}}</p>
@@ -9,7 +9,12 @@
 
 <script>
     export default{
-        props: ['header','text','theme']
+        props: ['header','text','theme'],
+        methods: {
+            closeModal(){
+                this.$emit('close') 
+            }
+  }
     }
 </script>
 
